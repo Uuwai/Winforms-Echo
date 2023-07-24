78d394c2cf04e190605aeb6d812af151b149b244
ウウワイ <118117530+Uuwai@users.noreply.github.com>
2023-07-24 15:33:52 +0800

    Add files via upload

---
+++ record and play audio/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace record_and_play_audio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ForceStop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Location = new System.Drawing.Point(13, 42);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            32767,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(74, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            25,
+            0,
+            0,
+            131072});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "ResetValue";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(179, 92);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Echo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ record and play audio/Form1.cs
@@ -0,0 +1,90 @@
+﻿using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace record_and_play_audio
+{
+    public partial class Form1 : Form
+    {
+        private WasapiLoopbackCapture loopbackCapture;
+        private WaveOutEvent waveOut;
+        private BufferedWaveProvider buffer;
+
+        private void InitializeAudioComponents()
+        {
+            loopbackCapture = new WasapiLoopbackCapture();
+
+            waveOut = new WaveOutEvent();
+
+            buffer = new BufferedWaveProvider(loopbackCapture.WaveFormat);
+
+            loopbackCapture.DataAvailable += (sender, e) =>
+            {
+                buffer.AddSamples(e.Buffer, 0, e.BytesRecorded);
+            };
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeAudioComponents();
+        }
+        private void StartRecordingAndPlayback()
+        {
+            loopbackCapture.StartRecording();
+
+            var volumeSampleProvider = new VolumeSampleProvider(buffer.ToSampleProvider());
+            volumeSampleProvider.Volume = (float)numericUpDown1.Value;
+            waveOut.Init(volumeSampleProvider);
+            waveOut.Play();
+        }
+
+        private void StopRecordingAndPlayback()
+        {
+            loopbackCapture.StopRecording();
+
+            waveOut.Stop();
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            loopbackCapture.StopRecording();
+
+            waveOut.Stop();
+        }
+        bool enabled = false;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopRecordingAndPlayback();
+            button1.Text = "Start";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (enabled == false)
+            {
+                StartRecordingAndPlayback();
+                enabled = true;
+                button1.Text = "Stop";
+            }
+            else
+            {
+                StopRecordingAndPlayback();
+                enabled = false;
+                button1.Text = "Start";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0.25m;
+        }
+    }
+}
